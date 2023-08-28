f731b47841c464c512ebafb2625c1f930684cc37
Коньков Данила <danila9807@ya.ru>
2023-08-28 15:06:41 +0300

    Наполняем базу данных новыми рандомными данными 50 штук

--- test/database/seeders/ConcurrencySeeder.php
+++ test/database/seeders/ConcurrencySeeder.php
@@ -11,47 +11,18 @@ class ConcurrencySeeder extends Seeder
 {
     public function run()
     {
+        $productCards = ProductCard::pluck('id')->shuffle();
+        $sourceItems = SourceItem::pluck('id')->shuffle();
 
-        Concurrency::create([
-           'product_card_id' => 1,
-           'source_item_id' => 1,
-        ]);
+        $concurrencyData = [];
 
-        Concurrency::create([
-            'product_card_id' => 1,
-            'source_item_id' => 2,
-        ]);
+        for ($i = 0; $i < 50; $i++) {
+            $concurrencyData[] = [
+                'product_card_id' => $productCards[$i],
+                'source_item_id' => $sourceItems[$i],
+            ];
+        }
 
-        Concurrency::create([
-            'product_card_id' => 1,
-            'source_item_id' => 3,
-        ]);
-
-//        $productCards = ProductCard::all();
-//        $sourceItems = SourceItem::all();
-//
-//        foreach ($productCards as $productCard) {
-//            foreach ($sourceItems as $sourceItem) {
-//                $existingPivot = $productCard->sourceItems()->where('source_item_id', $sourceItem->id)->first();
-//
-//                if (!$existingPivot) {
-//                    // Если цена товара null, то устанавливаем эту цену опт и розницы карточке товара.
-//                    // Если у карточки товара есть фиксированная цена, устанавливаем ее как розничную цену.
-//                    // Иначе оставляем цены прайслиста без изменений.
-//                    $wholesalePrice = $sourceItem->opt_price;
-//                    $retailPrice = $sourceItem->retail_price;
-//                    if ($productCard->retail_price !== null) {
-//                        $retailPrice = $productCard->retail_price;
-//                    } else {
-//                        $retailPrice = 0.00;
-//                    }
-//
-//                    $productCard->sourceItems()->attach($sourceItem, [
-//                        'wholesale_price' => $wholesalePrice,
-//                        'retail_price' => $retailPrice,
-//                    ]);
-//                }
-//            }
-//        }
+        Concurrency::insert($concurrencyData);
     }
 }
